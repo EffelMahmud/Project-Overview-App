f4c86da5a776685f59d390baf3577a083ce719ad
EffelMahmud <effel007@gmail.com>
2024-07-18 02:17:29 +0600

    Contact Title change

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ContactForm from "../components/contact-App/ContactForm";
 import Table from "../components/contact-App/Table";
+import { Helmet } from "react-helmet-async";
 
 export default function Contacts() {
   const [contacts, setContacts] = useState([]);
@@ -9,7 +10,9 @@ export default function Contacts() {
   };
   return (
     <div>
-      <h1>ContactApp</h1>
+      <Helmet>
+        <title>Contact</title>
+      </Helmet>
       <ContactForm getContact={getContact} />
       <br />
       <Table contacts={contacts} />
